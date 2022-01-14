162df3dc16fba077398fc7d7fbf0e7a7fc847a1a
AlisaBugaeva <alisabugaeva@mail.ru>
2022-01-14 19:02:54 +0300

    dto

---
+++ src/main/java/com/poputchiki/dto/registration/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.poputchiki.dto.registration;
+
+public class ErrorMessage {
+    private String errorMessage;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
--- src/main/java/com/poputchiki/dto/registration/RegistrationRequest.java
+++ src/main/java/com/poputchiki/dto/registration/RegistrationRequest.java
@@ -6,4 +6,44 @@ public class RegistrationRequest {
     private String telephone;
     private String email;
     private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
