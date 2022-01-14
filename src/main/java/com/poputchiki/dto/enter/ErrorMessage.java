28b24a44c26956382fdc90c205ca028ad3bb5101
AlisaBugaeva <alisabugaeva@mail.ru>
2022-01-14 18:59:13 +0300

    dto

---
+++ src/main/java/com/poputchiki/dto/enter/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.poputchiki.dto.enter;
+
+public class ErrorMessage {
+
+    private String errorMessage;
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
---
+++ src/main/java/com/poputchiki/dto/registration/RegistrationRequest.java
@@ -0,0 +1,9 @@
+package com.poputchiki.dto.registration;
+
+public class RegistrationRequest {
+    private String name;
+    private String surname;
+    private String telephone;
+    private String email;
+    private String password;
+}
