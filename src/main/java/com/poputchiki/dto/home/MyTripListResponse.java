009ef11767db2b947618fceb6918a38455a47a5c
AlisaBugaeva <alisabugaeva@mail.ru>
2022-01-14 19:06:55 +0300

    dto

---
+++ src/main/java/com/poputchiki/dto/home/MyTripListResponse.java
@@ -0,0 +1,42 @@
+package com.poputchiki.dto.home;
+
+import java.util.Date;
+
+public class MyTripListResponse {
+    private String start;
+    private String finish;
+    private Date startDate;
+    private Date finishDate;
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+
+    public void setFinish(String finish) {
+        this.finish = finish;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+}
---
+++ src/main/java/com/poputchiki/dto/home/NewTripListResponse.java
@@ -0,0 +1,60 @@
+package com.poputchiki.dto.home;
+
+import java.util.Date;
+
+public class NewTripListResponse {
+    private String name;
+    private String surname;
+    private String start;
+    private String finish;
+    private Date startDate;
+    private Date finishDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getFinish() {
+        return finish;
+    }
+
+    public void setFinish(String finish) {
+        this.finish = finish;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+}
